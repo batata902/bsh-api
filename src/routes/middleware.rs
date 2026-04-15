d9f66597e8410882c1dcae0389642d3f499aef65
JeRM <79025708+batata902@users.noreply.github.com>
2026-04-15 00:35:54 -0300

    Update middleware.rs

--- src/routes/middleware.rs
+++ src/routes/middleware.rs
@@ -1,19 +1,18 @@
-use axum::{http::{Request, StatusCode}, middleware::Next, response::Response};
-use crate::{models::models::AuthUser, utils::*};
+use axum::{http::{Request, StatusCode, header}, middleware::Next, response::Response};
+use crate::{models::models::{AuthUser}, utils::*};
 use std::env;
 
 pub async fn auth(mut req: Request<axum::body::Body>, next: Next) -> Result<Response, StatusCode> {
-    let has_token = req.headers().get("authorization");
-
+    let has_token = req.headers().get(header::AUTHORIZATION);
+    
     println!("{}: {}", req.method(), req.uri());
 
-    // Padrão nos headers -> Authorization: Bearer TOKEN
     if let Some(token) = has_token {
         let header_str = token.to_str().unwrap_or("");
 
         if header_str.starts_with("Bearer ") {
-            // Remove o "Bearer " da string
-            let token = header_str.trim_start_matches("Bearer "); 
+            let token = header_str.trim_start_matches("Bearer ");
+
             match check_jwt(token, env::var("JWT_SECRET").unwrap()) {
                 Ok(claims) => {
                     let id = claims.sub.parse::<i64>().map_err(|_| StatusCode::UNAUTHORIZED)?;
@@ -24,8 +23,7 @@ pub async fn auth(mut req: Request<axum::body::Body>, next: Next) -> Result<Resp
                 },
                 Err(_) => Err(StatusCode::UNAUTHORIZED)
             }
-        }
-        else {
+        } else {
             Err(StatusCode::UNAUTHORIZED)
         }
     } else {
